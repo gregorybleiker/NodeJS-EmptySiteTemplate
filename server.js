9ba3e261e039d9089b73446347bd3d3ebbe96e8b
Gregory Bleiker <gregorybleiker@bbv.ch>
2018-07-05 16:56:51 +0200

    graphql impl

--- server.js
+++ server.js
@@ -1,3 +1,42 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
+
+const resolvers = {
+  Query: {
+    sePomodoros: (_, args, context, info) => {
+      return context.prisma.query.pomodoroes(
+        {
+          where: {
+            name_contains: args.searchString
+          }
+        },
+        info
+      );
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: "src/schema.graphql",
+  resolvers,
+  context: req => ({
+    ...req,
+    prisma: new Prisma({
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint: "https://eu1.prisma.sh/gregory-bleiker-1224dd/prisma_sample/dev"
+    })
+  })
+});
+const options = {
+  port: 8080,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/playground',
+}
+server.start(options, () =>
+  console.log(`GraphQL server is running on http://localhost:8080`)
+);
+/*
 var http = require('http');
 
 http.createServer(function (req, res) {
@@ -6,3 +45,4 @@ http.createServer(function (req, res) {
     res.end('Hello, world from me too!');
     
 }).listen(process.env.PORT || 8080);
+*/
