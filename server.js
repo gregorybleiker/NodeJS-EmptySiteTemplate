29021cd056bc7780a1b7124575b81973e0799140
Gregory Bleiker <gregorybleiker@bbv.ch>
2018-07-05 15:45:20 +0200

    int test

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world from me!');
+    res.end('Hello, world from me too!');
     
 }).listen(process.env.PORT || 8080);
